57cb352c7a8903b8266d1fe5bafade97a4c5f77a
agmatyagi1717 <agamtyagi1717@gmail.com>
2024-03-03 15:26:27 +0530

    initialcommit

--- app/page.tsx
+++ app/page.tsx
@@ -70,6 +70,7 @@ export default function Home() {
 
   return (
     <div>
+      <h1>Gemini Pro api testing</h1>
       <h1>Enter your code: </h1>
       <textarea
         value={code}
@@ -81,7 +82,7 @@ export default function Home() {
         onClick={run}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
-        Get Code
+        Get Code in C++
       </button>
       <p>{changedCode}</p>
     </div>
